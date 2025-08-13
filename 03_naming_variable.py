16c7e447b7c524a115bb90bfc243172d300a9fd5
hariskhan2010 <harismalik2010afridi@gmail.com>
2025-08-13 10:44:10 +0500

    Add files via upload

---
+++ 03_naming_variable.py
@@ -0,0 +1,17 @@
+first_name = "Haris"       #snake_case
+firstName = "Haris Khan"        #camelCase
+FirstName = "Haris"        #PascalCase
+
+
+case = ("firstName" == "FirstName")
+print(case)
+
+
+firstName = "Hasim"
+full_name = "Haris"         #enter your full name
+sn = "Samad"
+
+first_name = "Haris"
+first_name = "Abdul"
+
+print(first_name)
